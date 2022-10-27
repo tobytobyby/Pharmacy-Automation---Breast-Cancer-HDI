ef0188d155d1bc8945ef82cdc70bb3f3749aabc4
tobytobyby <62234103+tobytobyby@users.noreply.github.com>
2022-10-27 12:39:34 +0800

    Created FinSynonyms.py
    The synonyms found are saved in synonyms.csv. However, due to some typo in the original .xlsx, synonyms of some drugs are not found yet.

---
+++ FindSynonyms.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# Libraries 
+import sys
+import os
+import pandas as pd
+import pubchempy as pcp
+import time
+import csv
+
+Infile = 'Breast Drug List.xlsx'
+df = pd.read_excel(Infile, engine='openpyxl')
+
+lst_drug = df.values.tolist()
+
+dict_synonyms = {}
+
+for drug in lst_drug:
+    drug = drug[0]
+    try:
+        df1 = pcp.get_synonyms(drug, 'name')
+
+        synonyms = df1[0]['Synonym']
+
+        dict_synonyms[drug] = synonyms
+
+        print(drug + ' is found')
+
+        time.sleep(1)
+    except:
+        print(drug + " is invalid drug")
+
+Outfile = 'synonyms.csv'
+with open(Outfile, 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in dict_synonyms.items():
+        writer.writerow([key, value])
+
+print('There are ' + str(len(dict_synonyms)) + ' drugs.')
+
+    
+    
+    
+
