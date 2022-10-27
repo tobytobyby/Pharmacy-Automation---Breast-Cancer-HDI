ba8dae322a58eb5850eb1dc8c9935e17957265c2
tobytobyby <62234103+tobytobyby@users.noreply.github.com>
2022-10-27 15:35:17 +0800

    updated Original Drug List xlsx
    Some are still missing, including those -mab and filgrastim, which have several entries on PubChem, and I don't know which to use.

--- FindSynonyms.py
+++ FindSynonyms.py
@@ -14,6 +14,7 @@ Infile = 'Breast Drug List.xlsx'
 df = pd.read_excel(Infile, engine='openpyxl')
 
 lst_drug = df.values.tolist()
+lst_unfound = []
 
 dict_synonyms = {}
 
@@ -26,19 +27,22 @@ for drug in lst_drug:
 
         dict_synonyms[drug] = synonyms
 
-        print(drug + ' is found')
+        # print(drug + ' is found')
 
         time.sleep(1)
     except:
         print(drug + " is invalid drug")
+        lst_unfound.append(drug)
 
 Outfile = 'synonyms.csv'
 with open(Outfile, 'w') as csv_file:
     writer = csv.writer(csv_file)
     for key, value in dict_synonyms.items():
         writer.writerow([key, value])
+    writer.writerow(['unfound',lst_unfound])
 
 print('There are ' + str(len(dict_synonyms)) + ' drugs.')
+
 
     
     
